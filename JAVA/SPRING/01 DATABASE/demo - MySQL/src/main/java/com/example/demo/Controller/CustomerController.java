44c37eb650954a76dd937f051a7ba19eac8c3ee5
davex-ai <omotoyedave4@gmail.com>
2026-05-13 13:10:35 +0100

    more code

---
+++ JAVA/SPRING/01 DATABASE/demo - MySQL/src/main/java/com/example/demo/Controller/CustomerController.java
@@ -0,0 +1,42 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Entity.Customer;
+import com.example.demo.Service.CustomerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomer(){
+        return customerService.getAllCustomer();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Integer id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer){
+       return customerService.createCustomer(customer);
+        }
+
+        @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Integer id, @RequestBody Customer customer){
+        return customerService.updateCustomer(id, customer);
+        }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Integer id){
+        customerService.deleteCustomer(id);
+    }
+    }
---
+++ JAVA/SPRING/05 API GATEWAY/order/src/main/java/com/dave/order/Service/OrderService.java
@@ -0,0 +1,42 @@
+package com.dave.order.Service;
+
+import com.dave.order.Entity.Order;
+import com.dave.order.Repository.OrderRepository;
+import com.dave.order.dto.Customer;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@AllArgsConstructor
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+    private RestTemplate restTemplate;
+    private  final String CUSTOMER_URL = "http://localhost:8080/customers";
+
+    public Customer createCustomer(Customer customer){ return restTemplate.postForObject(CUSTOMER_URL, customer, Customer.class);}
+    public void updateCustomer(Long id, Customer customer){ restTemplate.put(CUSTOMER_URL + "/" + id, customer);}
+    public void deleteCustomer(Long id){ restTemplate.delete(CUSTOMER_URL + "/" + id);}
+
+    public Order getOrderById(Long id){return orderRepository.findById(id).orElseThrow(() -> new RuntimeException("Not Fouund"));}
+
+    public Map<String, Object> createOrder(Order order){
+        String url = "http://localhost:8080/customers/" + order.getCustomerId();
+        Order savedOrder = orderRepository.save(order);
+        Customer customer = restTemplate.getForObject(url, Customer.class);
+
+        if (customer == null) {
+            throw new RuntimeException("Customer not found");
+        }
+        Map<String, Object> response = new HashMap<>();
+        response.put("order", savedOrder);
+        response.put("customer", customer);
+
+
+        return response;
+    }
+}
